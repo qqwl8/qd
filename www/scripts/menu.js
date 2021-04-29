19564d4d2aeb87136407ae8161e9957eb54746cc
liulaotou <lb_yx@163.com>
2021-04-29 16:38:50 +0800

    UP

---
+++ www/scripts/menu.js
@@ -0,0 +1,29 @@
+﻿$(function(){
+	  $(".header ul").mouseover(function(){
+    $(".cox").addClass("activ");
+  });
+  $(".header ul").mouseout(function(){
+    $(".cox").removeClass("activ");
+  });
+
+  $(".cox").mouseover(function(){
+    $(".cox").addClass("activ");
+  });
+  $(".cox").mouseout(function(){
+    $(".cox").removeClass("activ");
+
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
